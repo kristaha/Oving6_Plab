22d1fdadd2eee94658b8dab927427c599a2ba1ef
AugustLE <le_lund@hotmail.com>
2016-11-02 10:38:04 +0100

    OK

--- basic_robot/robodemo.py
+++ basic_robot/robodemo.py
@@ -45,7 +45,7 @@ def crashTest():
     while True:
 
         counter += 1
-        if counter >= 20:
+        if counter >= 5:
 
             f_value = sensor.calculateFront()
             ir_command = sensor.calculateSides()
