59a8e881925440765f6ebe7a0f8e8627e84602bf
martvo <martin.vold@hotmail.no>
2016-11-02 12:25:38 +0100

    t

--- basic_robot/robodemo.py
+++ basic_robot/robodemo.py
@@ -141,7 +141,7 @@ def cameraTest():
     s = 1
     im = IMR.Imager(image=camera.update()).scale(s,s)
     im = im.map_color_wta()
-    im.dump_image("/root/Oving6_Plab/basic_robot/bilder/test2.jpg")
+    im.dump_image("/root/Oving6_Plab/basic_robot/bilder/test2.png")
     return
 
 
