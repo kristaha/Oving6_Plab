14b1c6bc698831ba85b5a3cd69020886125372e2
Kristian Haga <Kristian@dhcp-10-22-33-138.wlan.ntnu.no>
2016-11-02 10:58:51 +0100

    fuck

--- basic_robot/FollowLine.py
+++ basic_robot/FollowLine.py
@@ -17,7 +17,7 @@ class FollowLine:
         nowValue = self.getValueList()
         offLineList = [0,1,4,5]
         for i in range(len(self.values)):
-            if i in offLineList and nowValue[i] > 500:
+            if i in offLineList and nowValue[i] > 400:
                 return [nowValue[i],i]
         return [0,10]
 
