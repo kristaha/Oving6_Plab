ab1fc65238a3acda3d3c0d5515e0afb169a07a26
AugustLE <le_lund@hotmail.com>
2016-11-02 13:00:28 +0100

    OK

--- basic_robot/Arbitrary.py
+++ basic_robot/Arbitrary.py
@@ -1,5 +1,6 @@
 from FollowLine import FollowLine
 from crash_sensor import Crash_sensor
+from processPic import ProcessPic
 
 
 class Arbitrary:
@@ -8,16 +9,16 @@ class Arbitrary:
 
         self.crash_sensor = Crash_sensor()
         self.follow_line = FollowLine()
-
-
+        self.camera_sensor = ProcessPic()
 
     def priority_sensor(self):
 
         pri_value_crash = self.crash_sensor.getPriValues()
         pri_value_follow = self.follow_line.isOnLine()
+        #pri_value_camera = self.camera_sensor...
 
-        value_list = [pri_value_crash[0], pri_value_follow[0]]
-        sens_list = [self.crash_sensor, self.follow_line]
+        value_list = [pri_value_crash[0], pri_value_follow[0]] # + pri_value_camera[0]
+        sens_list = [self.crash_sensor, self.follow_line] #  + self.camera_sensor
         highest_value = max(value_list)
 
         if highest_value == 0:
