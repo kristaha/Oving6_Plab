0ce6c81a9008015e204d613868e7693af692a3f7
martvo <martin.vold@hotmail.no>
2016-11-02 11:35:29 +0100

    Jad

--- basic_robot/robodemo.py
+++ basic_robot/robodemo.py
@@ -119,6 +119,8 @@ def shoot_panorama(shots=5):
         print("Kjør!!!!")
         motors.right(0.5,rotation_time)
         im = im.concat_horiz(IMR.Imager(image=camera.update()))
+    #im.dump_image("C:\\Users\\Martin\\PycharmProjects\\Semester3\\Proglab2\\
+    im.display()
     return im
 
 def followTest():
