cadea0aa9baee4c004ece75294998ac9aac7b20f
Kristian Haga <Kristian@dhcp-10-22-33-138.wlan.ntnu.no>
2016-11-02 13:33:12 +0100

    MotorAction

--- basic_robot/FollowLine.py
+++ basic_robot/FollowLine.py
@@ -7,6 +7,7 @@ class FollowLine:
     def __init__(self):
         self.reflectanseSensor = ReflectanceSensors()
         self.values = self.getValueList()
+        self. values = self.isOnLine()
 
     def getValueList(self):
         self.reflectanseSensor.reset()
@@ -20,6 +21,9 @@ class FollowLine:
             if i in offLineList and nowValue[i] < 0.2:
                 return [(1000-(nowValue[i]*1000)),i]
         return [0,10]
+
+    def getPriValues(self):
+        return self.isOnLine()
 
     # I arbitary klassen bruker jeg bare retur vediene til isOnLine() som prioriteringsverdier
     # Da blir det priverdier mellom 0-1000
---
+++ basic_robot/MotorAction.py
@@ -0,0 +1,84 @@
+from motors import Motors
+from Arbitrary import Arbitrary
+from types import *
+from FollowLine import FollowLine
+from crash_sensor import Crash_sensor
+
+class MotorAction():
+
+    def __init__(self):
+
+        self.arbit = Arbitrary()
+        self.value = None
+        self.motorList = []
+        self.motorList.append(driveMotor=Motors())
+
+    def UpdateValue(self):
+        self.value = self.arbit.priority_sensor()
+    def getValue(self):
+        return self.value
+
+#Handlinger ved forskjellige pri_sensorer
+    def crash(self, value):
+        if value == 'FORRWARD':
+            self.motorList[0].stop()
+            self.motorList[0].backward(0.2,0.4)
+        elif value == 'LEFT':
+            self.crashLeft()
+        else:
+            self.crashRight()
+
+    def crashLeft(self):
+        self.motorList[0].stop()
+        self.motorList[0].right(0.2,0.4)
+
+    def crashRight(self):
+        self.motorList[0].stop()
+        self.motorList[0].left(0.2, 0.4)
+
+    def followLine(self,indexOfSensor):
+
+        if indexOfSensor == 0 or indexOfSensor ==1:
+            self.motorList[0].left(0.2,0.3)
+        else:
+            self.motorList[0].right(0.2,0.3)
+
+    def camera(self,value):
+
+        if value ==1:
+            self.motorList[0].stop()
+        else:
+            self.motorList[0].forward(0.6,0.2)
+
+    def noPri(self):
+        self.motorList[0].forward(0.2,0.3)
+
+## Kjøre def
+    def drive(self):
+        time = 0
+        while( time < 300):
+            time += 1
+            self.UpdateValue()
+            value = self.getValue()
+
+            if value == None:
+                self.noPri()
+
+            elif type(value) == FollowLine():
+                self.followLine(value.getPriValue()[1])
+
+            elif type(value) == Crash_sensor:
+                self.crash(value.getPriValue()[1])
+            else:
+                self.camera(0)
+            
+
+
+
+
+
+
+
+
+
+
